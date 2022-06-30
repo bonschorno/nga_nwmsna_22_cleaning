f6887119105c94fd4b329363019bf5ab5076e341
bonschorno <colin.walder@hotmail.com>
2022-06-30 17:56:35 +0100

    delete db_access.R and merge into 01_get_data.R

--- 01_get_data.R
+++ 01_get_data.R
@@ -8,6 +8,7 @@ library(KoboconnectR)
 library(httr)
 library(tidyverse)
 library(lubridate)
+library(DBI)
 
 # Download data from Kobo -------------------------------------------------
 
@@ -42,12 +43,30 @@ df <- GET(new_export_url, authenticate(user=rstudioapi::askForPassword("username
 df_content <- content(df, type="raw",encoding = "UTF-8") 
 
 # path for most recent file
-most_recent_kobo_data <- paste0("raw_data/data_kobo_", date_today, ".csv")
+# most_recent_kobo_data <- paste0("raw_data/data_kobo_", date_today, ".csv")
+# writeBin(df_content, most_recent_kobo_data)
 
-writeBin(df_content, most_recent_kobo_data)
+writeBin(df_content, "raw_data/raw_kobo_data.csv")
 
-rm(df_content)
+rm(df_content, df)
 
+# Write to Postgres database ----------------------------------------------
+
+raw_kobo_data_csv <- read_delim("raw_data/raw_kobo_data.csv", delim = ";")
+
+connection <- dbConnect(RPostgres::Postgres(),
+                        host = "localhost",
+                        dbname = "postgres",
+                        user = rstudioapi::askForPassword("username"),
+                        password = rstudioapi::askForPassword("pw"))
+
+dbWriteTable(connection, 
+             'raw_kobo_data', 
+             raw_kobo_data_csv,
+             overwrite=TRUE,
+             row.names=FALSE)
+
+dbDisconnect(connection)
 
 # Create log_deletion file ------------------------------------------------
 
@@ -56,13 +75,13 @@ if(file.exists("final_output/final_deletionfile.csv") == TRUE){
   existing_deletion_ids <- read_csv("final_output/final_deletionfile.csv") %>% 
     pull(`_uuid`)
     
-  data_raw <- read_delim(most_recent_kobo_data) %>% 
+  data_raw <- read_delim("raw_data/raw_kobo_data.csv", delim = ";") %>% 
     filter(!`_uuid` %in% existing_deletion_ids)
   
   
 }else{
   
-  data_raw <- read_delim(most_recent_kobo_data)
+  data_raw <- read_delim("raw_data/raw_kobo_data.csv", delim = ";")
 }
 
 data_deletion <- data_raw %>% 
@@ -121,6 +140,7 @@ write_csv(data_pre_clean, "pre_clean_data/pre_clean_data.csv")
 # Visualize n surveys per day -----------------------------------------------------
 
 data_pre_clean %>% 
+  filter(today > as.Date("2021-01-01")) %>% 
   group_by(today) %>% 
   count() %>% 
   arrange(desc(today)) %>% 
